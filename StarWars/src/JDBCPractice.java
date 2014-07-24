da1a2618a5fae77651f91d283790d955a723946b
Yoonsung <lvev9925@naver.com>
2014-07-24 21:33:54 +0900

    Create CallableStatement Practice Souce

---
+++ StarWars/src/JDBCPractice.java
@@ -0,0 +1,35 @@
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Types;
+
+
+public class JDBCPractice {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection connection;
+		String user = "yoonsung";
+		String password = "yoondb";
+		String url = "jdbc:mysql://10.73.45.65/yoda?noAccessToProcedureBodies=true";
+		
+		String sql = "{CALL ADDUSER(?, ?, ?)}";
+		
+		connection = DriverManager.getConnection(url, user, password);
+		CallableStatement callableStatement = connection.prepareCall(sql);
+
+		callableStatement.registerOutParameter(1, Types.INTEGER);
+		callableStatement.registerOutParameter(2, Types.INTEGER);
+		callableStatement.registerOutParameter(3, Types.TINYINT);
+		
+		callableStatement.execute();
+		
+		int userId = (int) callableStatement.getObject(1);
+		int databaseId = (int) callableStatement.getObject(2);
+		int galaxyId = (int) callableStatement.getObject(3);
+		
+		System.out.println(userId);
+		System.out.println(databaseId);
+		System.out.println(galaxyId);
+	}
+}
